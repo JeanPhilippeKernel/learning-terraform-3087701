c3db1d028ebb4fe9066e806ff796841a58d03cbe
Jean Philippe <jeanphilippe02666592@outlook.fr>
2024-09-12 16:30:42 +0100

    fixed backend port

--- main.tf
+++ main.tf
@@ -63,10 +63,10 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
+      name_prefix       = "blog-"
+      backend_protocol  = "HTTP"
+      backend_port      = 80
+      target_type       = "instance"
     }
   ]
 
